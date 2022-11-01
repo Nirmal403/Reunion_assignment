efb8739f2692525c2a02b8f03f226f6b87e280ee
Nirmal403 <nirmalcool414@gmail.com>
2022-11-01 15:37:16 +0530

    navbar done static

--- reunion_assignment/src/App.js
+++ reunion_assignment/src/App.js
@@ -1,11 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
-import Navbar from './component/Navbar';
 import MainRoute from './component/MainRoute';
 function App() {
   return (
     <div className="App">
-      <Navbar/>
       <MainRoute/>
     </div>
   );
--- reunion_assignment/src/component/Navbar.jsx
+++ reunion_assignment/src/component/Navbar.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import styles from "../css/navbar.module.css"
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div id={styles.nav}>
+        <div id={styles.seg1}>
+        <img id={styles.icon} src="https://www.brandbucket.com/sites/default/files/logo_uploads/239930/estatery_0.png" />
+        <p>Rent</p>
+        <p>Buy</p>
+        <p>Sell</p>
+        <p></p>
+        <select id="manageproperty">
+            <option value="Manage Property">Manage Property</option>
+            <option value="Houses">Houses</option>
+            <option value="Mortage">Mortage</option>
+            <option value="Villa">Villa</option>
+        </select>
+        <select>
+            <option value="Resources">Resources </option>
+            <option value="">A</option>
+            <option value="">B</option>
+            <option value="">C</option>
+        </select>
+        </div>
+        <div id={styles.seg2}>
+            <button id={styles.login}>Login</button>
+            <button  id={styles.signup}>Sign up</button>
+        </div>
+    </div>
   )
 }
 
