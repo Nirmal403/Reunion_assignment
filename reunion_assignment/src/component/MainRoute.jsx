47f5d78d90e015c7c74bf3eae467546ff8e54e0f
Nirmal403 <nirmalcool414@gmail.com>
2022-11-02 12:41:00 +0530

    wishlist added

--- reunion_assignment/src/component/MainRoute.jsx
+++ reunion_assignment/src/component/MainRoute.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import Navbar from './Navbar'
-import HomePage from '../Pages/Homepage'
+import HomePage from '../Pages/Homepage';
+import { Route, Routes } from 'react-router-dom';
+import Favourite from '../Pages/Favourite';
 const MainRoute = () => {
   return (
     <div>
-        <Navbar/>
-        <HomePage/>
+        {/* <Navbar/> */}
+        
+        <Routes>
+
+        <Route path="/" element={<HomePage/>} />
+        <Route path="/favourite" element={<Favourite/>}/>
+    </Routes>
     </div>
   )
 }
--- reunion_assignment/src/component/Navbar.jsx
+++ reunion_assignment/src/component/Navbar.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import styles from "../css/navbar.module.css"
-
-const Navbar = () => {
+import { Link } from 'react-router-dom';
+const Navbar = ({count}) => {
   return (
     <div id={styles.nav}>
         <div id={styles.seg1}>
-        <img id={styles.icon} src="https://www.brandbucket.com/sites/default/files/logo_uploads/239930/estatery_0.png" />
+        <Link to="/"><img id={styles.icon} src="https://www.brandbucket.com/sites/default/files/logo_uploads/239930/estatery_0.png" /></Link>
         <p>Rent</p>
         <p>Buy</p>
         <p>Sell</p>
@@ -22,6 +22,12 @@ const Navbar = () => {
             <option value="">B</option>
             <option value="">C</option>
         </select>
+        </div>
+        <div id={styles.fav}>
+            
+        {/* <button> */}
+            <Link to="/favourite" id={styles.link}><img width="20px" src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c8/Love_Heart_symbol.svg/2250px-Love_Heart_symbol.svg.png"/> {count}</Link>
+            {/* </button> */}
         </div>
         <div id={styles.seg2}>
             <button id={styles.login}>Login</button>
